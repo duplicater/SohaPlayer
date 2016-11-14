39f6ec371ca2c051629b308f69951cd823f2f620
lecuong <cuongleviet@vccorp.vn>
2016-11-14 02:20:15 -0800

    1.0.5 update

--- Example/Pods/Target Support Files/Pods-SohaPlayer_Tests/Pods-SohaPlayer_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SohaPlayer_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_SohaPlayer_Tests
-@end
--- Example/Pods/Target Support Files/Pods-SohaPlayer_Tests/Pods-SohaPlayer_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_SohaPlayer_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_SohaPlayer_TestsVersionString[];
-
