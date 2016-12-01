f6d66369e57379a893d060b1d66e4273e22a0e27
lecuong <cuongleviet@vccorp.vn>
2016-12-01 00:55:48 -0800

    rename SHCacheManager

--- Example/SohaPlayer/NativeComponents.m
+++ Example/SohaPlayer/NativeComponents.m
@@ -14,6 +14,7 @@
 
 //#define URL_TEST @"http://hls.vcmedia.vn/kenh14/bzr5ienedy-xge47jt5qgx8sd3mcsu/2016/11/10/cauhon-bongda-1478773085927-eda5d.mp4"
 #define URL_TEST @"http://www.streambox.fr/playlists/x36xhzz/x36xhzz.m3u8"
+#define URL_TEST2 @"http://hls.vcmedia.vn/premierleague/video/20161107224108/100281921_1200_96.mp4"
 
 @interface NativeComponents () <NativeControlsDelegate, SHViewControllerDelegate>
 @property (weak, nonatomic) IBOutlet UIView *playerContainer;
@@ -54,7 +55,7 @@
 
 - (void)setupPlayer{
     
-    config = [[SHPlayerConfig alloc] initWithSourceUrl:URL_TEST appkey:nil secretKey:nil vid:nil];
+    config = [[SHPlayerConfig alloc] initWithSourceUrl:URL_TEST2 appkey:nil secretKey:nil vid:nil];
     
     _player = [[SHViewController alloc] initWithConfiguration:config webRq:NO];
     
@@ -66,34 +67,6 @@
     
     [self.playerContainer insertSubview:_player.view atIndex:0];
     
-    /*
-    _item = [[AVPlayerItem alloc] initWithURL:[NSURL URLWithString:URL_TEST]];
-    _avPlayer = [[AVPlayer alloc] initWithPlayerItem:_item];
-    
-    _layerPlay = [AVPlayerLayer playerLayerWithPlayer:_avPlayer];
-    
-    [_layerPlay setFrame:self.playerContainer.bounds];
-    
-    //[_layerPlay setPlayer:_avPlayer];
-    
-    [_avPlayer play];
-    
-    [self.playerContainer.layer addSublayer:_layerPlay];
-    */
-    
-//    [_player addSHPlayerEventListener:@"durationChange" eventID:@" id_1" handler:^(NSString *eventName, NSString *params) {
-//        ///new duration value
-//        
-//        NSLog(@"event nname %@ \n params %@",eventName,params);
-//    }];
-//    
-//    
-//    [_player addSHPlayerEventListener:@"rootResize" eventID:@"face_id_2" handler:^(NSString *eventName, NSString *params) {
-//        /// the player parent was resized
-//        
-//        NSLog(@"event nname %@ \n params %@",eventName,params);
-//    }];
-    
 }
 
 - (void)updateCurrentPlaybackTime:(double)currentPlaybackTime{
@@ -104,7 +77,7 @@
 
 - (void)SHPlayer:(SHViewController*)player playerLoadStateDidChange:(SHMediaLoadState)state
 {
-    NSLog(@"playerLoadStateDidChange %lu",(unsigned long)state);
+    NSLog(@"player Load State Did Change %lu",(unsigned long)state);
 }
 - (void)SHPlayer:(SHViewController*)player playerPlaybackStateDidChange:(SHMediaPlaybackState)state
 {
